8bc310cf066bb3354957ec37fa5d785e894a0057
gwentgod <ziyaozhu@connect.hku.hk>
2022-06-07 18:47:06 +0800

    Modify planner API for obstacle free detection

--- planner/rrt_2d.py
+++ planner/rrt_2d.py
@@ -1,20 +1,16 @@
 import numpy as np
 
 from planner.src.rrt.rrt import RRT
-from planner.src.search_space.search_space import SearchSpace
 from planner.src.utilities.plotting import Plot
 
 
-Q = np.array([(150, 120)])  # length of tree edges
+Q = np.array([(150, 80)])  # length of tree edges
 r = 1  # length of smallest edge to check for intersection with obstacles
 max_samples = 99999 # max number of samples to take before timing out
 prc = 0.1  # probability of checking for a connection to goal
-X_dimensions = np.array([(0, 8080), (0, 4480)])  # dimensions of Search Space
 
 
-def plan(obstacles, init, goal):
-    X = SearchSpace(X_dimensions, obstacles)
-    # create rrt_search
+def plan(X, init, goal):
     init = (init[0]*1000, init[1]*1000)
     goal = (goal[0]*1000, goal[1]*1000)
     rrt = RRT(X, Q, init, goal, max_samples, r, prc)
