19ff57a5faf814ab0ffcaac02d65dee82334e248
gwentgod <ziyaozhu@connect.hku.hk>
2022-07-26 23:24:01 -0700

    tunning

--- planner/rrt_2d.py
+++ planner/rrt_2d.py
@@ -4,7 +4,7 @@ from planner.src.rrt.rrt import RRT
 from planner.src.utilities.plotting import Plot
 
 
-Q = np.array([(135, 75)])  # length of tree edges
+Q = np.array([(155, 125)])  # length of tree edges
 r = 1  # length of smallest edge to check for intersection with obstacles
 max_samples = 5000 # max number of samples to take before timing out
 prc = 0.2  # probability of checking for a connection to goal
